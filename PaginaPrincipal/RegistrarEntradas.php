1cd3fd76e272cc04b608bd611a5d238f633b5a25
Thomas-2024 <tomllgyy40@gmail.com>
2024-09-12 17:34:51 -0500

    Add files via upload

--- PaginaPrincipal/RegistrarEntradas.php
+++ PaginaPrincipal/RegistrarEntradas.php
@@ -7,6 +7,10 @@
     Nuevo: <input type="radio" name="opcion_producto" id="" value="nuevo" onchange="Mostrar(this.value, 'producto')">
     Existente <input type="radio" name="opcion_producto" id="" value="existente" onchange="Mostrar(this.value, 'producto')">
     <input type="text" name="Producto" disbaled style="display: none;" id="nuevo_producto">
+    <div>
+        <label>Precio</label>
+        <input type="text" name="Precio" id="Precio">
+    </div>
     <select name="Producto" disbaled style="display: none;" id="existente_producto">
         <?php
             require_once "../Uso_multiple/MostrarProductos.php";
@@ -27,7 +31,13 @@
        ?>
     </select>
     <label for="Estado">Estado: </label>
-    <input type="text" name="Estado" id="Estado">
+    <select name="Estado" id="estado">
+        <?php
+            require_once "../Uso_multiple/MostrarEstados.php";
+            $lista_estados = showEstados();
+            printf($lista_estados);
+       ?>
+    </select>
     <label for="Cantidad">Cantidad: </label>
     <input type="text" name="Cantidad" id="Cantidad">
     <select name="Unidad_de_medida" id="">
@@ -53,7 +63,13 @@
        ?>
     </select>
     <label for="Ubicacion">Ubicacion: </label>
-    <input type="text" name="Ubicacion" id="Ubicacion">
+    <select name="Ubicacion" id="ubicacion">
+        <?php
+            require_once "../Uso_multiple/MostrarUbicacionesDisponibles.php";
+            $lista_ubicaciones_disponibles = showUbicacionesDisponiles();
+            printf($lista_ubicaciones_disponibles);
+       ?>
+    </select>
     <input type="submit" name="RegistrarLote" value="Registrar Lote">
 </form>
 <script>
@@ -64,6 +80,13 @@
             document.getElementById("nuevo_"+opcion).disabled = false;
             document.getElementById("existente_"+opcion).style.display="none";
             document.getElementById("existente_"+opcion).disabled = true;
+            if(opcion == "producto"){
+                document.getElementById("Precio").style.display="block";
+                document.getElementById("Precio").disabled = false;
+            }else{
+                document.getElementById("Precio").style.display="none";
+                document.getElementById("Precio").disabled = true;
+            }
             if(opcion == "categoria"){
                 document.getElementById("abreviatura_categoria").style.display="block";
                 document.getElementById("abreviatura_categoria").disabled = false; 
@@ -86,20 +109,44 @@
     $conexion=MiConexion();
     if(isset($_POST['RegistrarLote'])){
         include "../Uso_multiple/CapturarDatosLotes.php";
-        $buscar_producto = $conexion->prepare("SELECT * FROM inventario");
-        $buscar_producto->execute();
-        
-        $sentencia_registrar =$conexion->prepare("INSERT INTO inventario (id_producto, Nombre, id_categoria, Precio, Stock, id_unidad_de_medida, Imagen) VALUES (:id_producto, :Nombre, :id_categoria, :Precio, :Stock, :id_unidad_de_medida, :Imagen)");
-        $sentencia_registrar->execute(
+        //insertar todos los datos en la tabla de lotes
+        $sentencia_registrar_lote = $conexion->prepare("INSERT INTO lotes VALUES (:id_lote, :id_producto, :id_categoria, :id_estado, :cantidad, :id_unidad_de_medida, :fecha_elaboracion, :fecha_expiracion, :id_proveedor, :id_ubicacion)");
+        $sentencia_registrar_lote->execute(
             array(
+                ':id_lote' => $Id_Lote,
                 ':id_producto' => $id_producto,
-                ':Nombre' => $Producto,
-                ':id_categoria' => $Categoria,
-                ':Precio' => 0,
-                ':Stock' => $Cantidad,
-                ':id_unidad_de_medida' => $Unidad_de_medida,
-                ':Imagen' => "imagenes/imagen.jpg"
+                ':id_categoria' => $id_categoria,
+                ':id_estado' => $id_estado,
+                ':cantidad' => $Cantidad,
+                ':id_unidad_de_medida' => $id_unidad_de_medida,
+                ':fecha_elaboracion' => $Fecha_Elaboracion,
+                ':fecha_expiracion' => $Fecha_Expiracion,
+                ':id_proveedor' => $id_proveedor,
+                ':id_ubicacion' => $id_ubicacion
             )
         );
+
+        $buscar_producto = $conexion->prepare("SELECT * FROM inventario WHERE id_producto = :id_producto");
+        $buscar_producto->execute(array(":id_producto"=>$id_producto));
+        $producto = $buscar_producto->fetch(PDO::FETCH_ASSOC);
+
+        if($producto){
+            $nueva_cantidad = $producto['cantidad']+$cantidad;
+            $actualizar_cantiodad = $conexcion->prepare("UPDATE inventario SET cantidad = :cantidad WHERE id_producto = :id_producto");
+            $actualizar_cantiodad->execute(array(":cantidad"=>$nueva_cantidad, ":id_producto"=>$id_producto));
+        }else {
+            $sentencia_registrar =$conexion->prepare("INSERT INTO inventario (id_producto, Nombre, id_categoria, Precio, Stock, id_unidad_de_medida, Imagen) VALUES (:id_producto, :Nombre, :id_categoria, :Precio, :Stock, :id_unidad_de_medida, :Imagen)");
+            $sentencia_registrar->execute(
+                array(
+                    ':id_producto' => $id_producto,
+                    ':Nombre' => $producto,
+                    ':id_categoria' => $categoria,
+                    ':Precio' => $precio,
+                    ':Stock' => $cantidad,
+                    ':id_unidad_de_medida' => $id_unidad_de_medida,
+                    ':Imagen' => $dir_imagen
+                )
+            );
+        }
     }
 ?>
