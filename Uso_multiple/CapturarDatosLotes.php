acf4d1c1db45e1aab5f8f5c49bcd00649cfd7c29
Thomas-2024 <tomllgyy40@gmail.com>
2024-09-14 17:46:36 -0500

    Add files via upload

--- Uso_multiple/CapturarDatosLotes.php
+++ Uso_multiple/CapturarDatosLotes.php
@@ -1,6 +1,30 @@
 <?php
     //se van a capturar los datos de id del lote, producto, categoria, estado, cantidad, unidad de medida, fecha de elaboracion, fecha de expiracion, proveedor y ubicacion
-
+    function capturar($ext, $allow, $error, $size, $tmp, $name, $categoria_producto) {
+        $Img_producto_Dest = "";
+        if (empty($Img_producto)){
+            if (!empty($_POST['Img_productoOld'])){
+                $Img_producto_Dest = $_POST['Img_productoOld'];
+            }
+        }
+        if (in_array($ext, $allow)) {
+            if ($error === 0) {//
+                if(file_exists('../imagenesdeproductos/'.$categoria_producto)){
+                    $Img_producto_Dest = '../imagenesdeproductos/'.$categoria_producto.'/ '.$name;
+                    move_uploaded_file($tmp, $Img_producto_Dest);
+                } else {
+                    mkdir('../imagenesdeproductos/'.$categoria_producto, 0777, true);
+                    $Img_producto_Dest = '../imagenesdeproductos/'.$categoria_producto.'/ '.$name;
+                    move_uploaded_file($tmp, $Img_producto_Dest);
+                }
+            } else {
+                echo "Hubo un error al subir la imagen";
+            }
+        } else {
+            echo "Solo puedes subir archivos de tipo (jpg, jpeg, png y pdf)";
+        }    
+        return $Img_producto_Dest;
+    }
     $Id_Lote = $_POST['Id_Lote'];
     $opcion_producto = $_POST['opcion_producto'];
     if($opcion_producto == "nuevo"){
@@ -8,11 +32,12 @@
         $precio = $_POST['Precio'];
         $id_unidad_de_medida = $_POST['Unidad_de_medida'];
         $opcion_categoria = $_POST['opcion_categoria'];
-        $categoria = $_POST['Categoria'];
+
         if($opcion_categoria == "nuevo"){
+            $nombre_categoria = $_POST['Categoria'];
             $abreviatura_categoria = $_POST['Abreviatura_Categoria'];
             $insertar_categoria = $conexion->prepare("INSERT INTO categoria (categoria, abreviatura) VALUES(:categoria, :abreviatura)");
-            $insertar_categoria->execute(array(":categoria"=>$categoria, ":abreviatura"=>$abreviatura_categoria));
+            $insertar_categoria->execute(array(":categoria"=>$nombre_categoria, ":abreviatura"=>$abreviatura_categoria));
 
             $buscar_ultima_categoria = $conexion->prepare("SELECT * FROM categoria ORDER BY id_categoria DESC LIMIT 1");
             $buscar_ultima_categoria->execute();
@@ -24,7 +49,25 @@
             $buscar_categoria->execute(array('id_categoria' => $id_categoria));
             $categoria = $buscar_categoria->fetch(PDO::FETCH_ASSOC);
             $abreviatura_categoria = $categoria['abreviatura'];
+            $nombre_categoria = $categoria['categoria'];
         }
+        //subir imagen del producto en la carpeta correspondiente
+        $Img_producto = $_FILES['Img_producto'];
+        $Img_producto_Name = $_FILES['Img_producto']['name'];
+        $Img_producto_TmpName = $_FILES['Img_producto']['tmp_name'];
+        $Img_producto_Size = $_FILES['Img_producto']['size'];
+        $Img_producto_Error = $_FILES['Img_producto']['error'];
+        $Img_producto_Type = $_FILES['Img_producto']['type'];
+    
+        $Img_producto_Ext = explode('.', $Img_producto_Name);//explode es una funcion que divide un string en varios strings: en este caso divide el nombre de la imagen en un array de strings si la imagen se llama image.jpg entonces el array quedaria asi: [image, jpg]
+        for ($i=0; $i < count($Img_producto_Ext); $i++) { 
+            echo $Img_producto_Ext[$i]."<br>";
+        }
+        $Img_producto_Actual_Ext = strtolower(end($Img_producto_Ext));
+    
+        $allowed = array("jpg", "jpeg", "png", "pdf");
+        $dir_img_producto = capturar($Img_producto_Actual_Ext, $allowed, $Img_producto_Error, $Img_producto_Size, $Img_producto_TmpName, $Img_producto_Name, $nombre_categoria);
+        
 
         // Obtener el último ID del producto de la misma categoría
         $buscar_ultima_id = $conexion->prepare("SELECT MAX(id_producto) AS max_id FROM inventario WHERE id_producto LIKE :prefix");
@@ -45,13 +88,14 @@
         $id_producto = $abreviatura_categoria.str_pad($siguienteNumeroID, 6, '0', STR_PAD_LEFT);
 
         //Insertar el nuevo producto en la tabla producto
-        $insertar_producto = $conexion->prepare("INSERT INTO producto (id_producto, nombre_producto, precio, id_unidad_de_medida, id_categoria) VALUES (:id_producto, :nombre_producto, :precio, :id_unidad_de_medida, :id_categoria)");
+        $insertar_producto = $conexion->prepare("INSERT INTO producto (id_producto, nombre_producto, precio, id_unidad_de_medida, id_categoria, Imagen) VALUES (:id_producto, :nombre_producto, :precio, :id_unidad_de_medida, :id_categoria, :Imagen)");
         $insertar_producto->execute(array(
             ':id_producto' => $id_producto,
             ':nombre_producto' => $nombre_producto,
             ':precio' => $precio,
             ':id_unidad_de_medida' => $id_unidad_de_medida,
-            ':id_categoria' => $id_categoria
+            ':id_categoria' => $id_categoria,
+            ':Imagen' => $dir_img_producto
         ));
     }else{
         $id_producto = $_POST['Producto'];
